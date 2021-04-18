d2c650e11d0eeb6378889ec0c39264f52265e774
aditya <aazadhind1906@gmail.com>
2021-04-18 21:59:35 +0530

    pasowrd eye added successfully

---
+++ assets/js/navigation.js
@@ -0,0 +1,24 @@
+
+
+const eye = document.querySelector('#eye');
+const passwordinput = document.querySelector('#modalLRInput11');
+
+eye.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = passwordinput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordinput.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+
+const eye2 = document.querySelector('#eye2');
+const passwordregister = document.querySelector('#modalLRInput16');
+
+eye2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = passwordregister.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordregister.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
